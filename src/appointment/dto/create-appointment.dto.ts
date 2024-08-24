2bd32f8ea958e05e989f2e4e4a1c9d061ca91fbe
1josee <trnnhattien@gmail.com>
2024-08-24 21:09:58 +0700

    finish appoinment feature and adjust schema.prisma

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAppointmentDto {}
+export class CreateAppointmentDto {
+  meeting_date: Date | string;
+  purpose: string;
+  start_time: Date | string;
+  end_time: Date | string;
+  location?: string | null;
+  meeting_link?: string | null;
+  meeting_status?: boolean | null;
+  p_id: number;
+  s_id: number;
+}
