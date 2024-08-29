16ab4d2e8fb7d8db442afd3e47459393458eb9e4
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-08-29 14:52:26 +0700

    feat: staff module dto creation and update/ add nestjs-uuid depen

--- src/staff/dto/create-staff.dto.ts
+++ src/staff/dto/create-staff.dto.ts
@@ -1,1 +1,10 @@
-export class CreateStaffDto {}
+import { UUID } from "crypto";
+
+export class CreateStaffDto {
+    s_id : UUID | number;
+    job_id : UUID | number;
+    dept_id : UUID | number;
+    manager_id : UUID | number;
+    qualifications : string;
+    salary : number;
+}
